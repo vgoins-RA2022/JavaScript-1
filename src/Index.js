611e9044fe5c765adaa727208890aefd6f960491
VALERIE GOINS <valerie.goins56@gmail.com>
2023-01-26 18:46:44 -0700

    first commit

---
+++ src/Index.js
@@ -0,0 +1,11 @@
+// a javascript function that takes three(3) parameters (name, height and country)
+
+// writes an alert that prints out the inputs
+function printUserInput(name, height, country) {
+    console.log("Name: " + name);
+    console.log("Height: " + height);
+    console.log("Country: " + country);
+    alert("Name: " + name + "\nHeight: " + height + "\nCountry: " + country);
+  }
+  
+  printUserInput("Mathis Jones","6.2","United States");
